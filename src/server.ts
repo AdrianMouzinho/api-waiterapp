4ab2885eb51968ae4a3ccf87eb5aae810feee18f
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2022-11-18 14:48:05 -0300

    aula 01

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import path from 'node:path';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { router } from './router';
+
+mongoose.connect('mongodb+srv://adrianmouzinho:cx12nvHe59XGSWZb@waiterapp.58hmeu7.mongodb.net/test')
+  .then(() => {
+    const app = express();
+    const port = 3000;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`);
+    })
+  })
+  .catch(() => console.log('erro ao conectar ao mongodb'));
+
